adc8db48ff89620099f979b7654584a59aa30868
VladimirK37 <61294220+VladimirK37@users.noreply.github.com>
2022-02-27 00:57:24 +0300

    Добавьте файлы проекта.

---
+++ AspNetCoreService/Data/PostsRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreService.Data
+{
+    internal static class PostsRepository
+    {
+        internal async static Task<List<Post>> GetPostsAsync()
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.ToListAsync();
+            }
+        }
+
+        internal async static Task<Post> GetPostsByIdAsync(int postId)
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.FirstOrDefaultAsync(post => post.PostId == postId );
+            }
+        }
+
+        internal async static Task<bool> CreatePostAsync(Post postIdCreate)
+        {
+            using (var db = new AppDbContext())
+            {
+                try 
+                {
+                    await db.Posts.AddAsync(postIdCreate);
+                    
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex) 
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> UpdatePostAsync(Post postToUpdate)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    db.Posts.Update(postToUpdate);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> UpdatePostAsync(int postId)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    Post postToDelete = await GetPostsByIdAsync(postId);
+
+                    db.Remove(postToDelete);    
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
