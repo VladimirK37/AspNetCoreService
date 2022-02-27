d288f6658c8aeaadb04427abe0f0886db01f9b4b
VladimirK37 <61294220+VladimirK37@users.noreply.github.com>
2022-02-27 17:53:29 +0300

    Добавление интерфеса н React

--- AspNetCoreService/Program.cs
+++ AspNetCoreService/Program.cs
@@ -3,6 +3,16 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder =>
+    {
+        builder.AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+    });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +32,8 @@ app.UseSwaggerUI(swaggerUIOptions =>
 });
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
 
 app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
     .WithTags("GetAllPosts");
